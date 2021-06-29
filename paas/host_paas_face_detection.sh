efca543abf8d0a4481ac0677fecffc1c160e1926
Chua, Vui Seng <vui.seng.chua@intel.com>
2021-06-28 20:56:14 -0700

    update host_paas_face_detection.sh for standalone image

--- paas/host_paas_face_detection.sh
+++ paas/host_paas_face_detection.sh
@@ -6,7 +6,7 @@ export workload=facedet
 export config=/workspace/nncf/paas/cfg/face-detection-0200/face-detection-0200.filter_pruning.yaml
 
 # Pls revise path to dataset
-export data=/data/dataset/WiderFace
+export data=/workspace/WiderFace
 
 # Pls revise path to pretrained model
 # Download from https://download.01.org/opencv/openvino_training_extensions/models/object_detection/v2/face-detection-0200.pth
